18f446a44b4ee81fad0a151f09fde673e156e5f8
Frank Barrera | farkbarn <frankb2320@gmail.com>
2015-07-31 14:51:52 -0430

    modificado dirección a authores y cargando contenido a la vista

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,9 @@ router.get('/', function(req, res) {
 
 
 router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer',   quizController.answer);
+router.get('/quizes/answer', quizController.answer);
+router.get('/author', quizController.author);
+
+
 
 module.exports = router;
