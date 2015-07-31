49ca1d70458fe2e0d29293f49e7808f6bf061019
Frank Barrera | farkbarn <frankb2320@gmail.com>
2015-07-31 16:51:28 -0430

    actualizando controlador

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,18 +1,27 @@
+var models = require('../models/models.js');
+
+
 // GET /quizes/question
 exports.question = function(req, res) {
-	res.render('quizes/question',{pregunta: 'Capital de Italia'});
+    models.Quiz.findAll().success(function(quiz){
+	res.render('quizes/question',{pregunta: quiz[0].pregunta});
+    })
 };
 
 // GET /quizes/answer
 exports.answer = function(req, res) {
-	if (req.query.respuesta === 'Roma' || req.query.respuesta === 'roma' ){
+    models.Quiz.findAll().success(function(quiz){
+	if (req.query.respuesta === quiz[0].respuesta){
 		res.render('quizes/answer',{respuesta: 'Correcto'});
 	} else {
 		res.render('quizes/answer',{respuesta: 'Incorrecto'});
 	}
+    })
 };
 
 // GET /author
 exports.author = function(req, res){
 	res.render('author', {author:'@farkbarn', foto:'/images/foto.jpg'});
 }
+
+
