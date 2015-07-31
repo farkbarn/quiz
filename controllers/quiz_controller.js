3a4a0718aa59662627cd3e991247448373c460cf
Frank Barrera | farkbarn <frankb2320@gmail.com>
2015-07-31 14:40:59 -0430

    agregando controlador para author

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -11,3 +11,8 @@ exports.answer = function(req, res) {
 		res.render('quizes/answer',{respuesta: 'Incorrecto'});
 	}
 };
+
+// GET /author
+exports.author = function(req, res){
+	res.render('author', {author:'@farkbarn'});
+}
