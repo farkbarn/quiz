89de504724eeb469fd44e2f06b1405db50bc45e9
Frank Barrera | @farkbarn <frankb2320@gmail.com>
2015-08-01 18:44:27 -0430

    reparando models.js

--- models/models.js
+++ models/models.js
@@ -2,8 +2,8 @@ var path = require('path');
 //Postgres: postgres://user:passwd@host:port/database
 //SQLite: sqlite://:@:/
 
-var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
-//var url = process.env.DATABASE_URL.match((.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+//var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var url = process.env.DATABASE_URL.match((.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
 var DB_name = (url[6]||null);
 var user    = (url[2]||null);
 var pwd     = (url[3]||null);
