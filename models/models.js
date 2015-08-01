b032d424b8daabfced9962cc4b13ef33e93566b0
Frank Barrera | @farkbarn <frankb2320@gmail.com>
2015-08-01 17:30:48 -0430

    conectando con PostgresSQL

--- models/models.js
+++ models/models.js
@@ -1,15 +1,39 @@
 var path = require('path');
 
+//Postgres: postgres://user:passwd@host:port/database
+//SQLite: sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6]||null);
+var user    = (url[2]||null);
+var pwd     = (url[3]||null);
+var protocol= (url[1]||null);
+var dialect = (url[1]||null);
+var port    = (url[5]||null);
+var host    = (url[4]||null);
+var storage = process.env.DATABASE_STORAGE;
+
 // cargar model ORM
 var Sequelize = require('sequelize');
 
-// usar BBDD SQLite_3
-var sequelize = new Sequelize(null,null,null,
-	{dialect: "sqlite", storage: "quiz.sqlite"}
+// Usar BBDD SQLite o Postgres	
+var sequelize = new Sequelize (DB_name, user, pwd,
+	{dialect:  protocol,
+	 protocol: protocol,
+	 port:	   port,
+	 host:     storage, //solo SQLite (.env)
+	 omitNull: true	// solo Postgres
+	}
 );
 
+// usar BBDD SQLite_3
+//var sequelize = new Sequelize(null,null,null,
+//	{dialect: "sqlite", storage: "quiz.sqlite"}
+//);
+
 // Import definición de la tabla Quiz en quiz.js
+var quiz_path = path.join(__dirname,'quiz');
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+
 
 exports.Quiz = Quiz; //exportando definición de tabla quiz
 
