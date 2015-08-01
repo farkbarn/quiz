96890d6a9b29e131a04ebd643aea84ecaeb04f82
Frank Barrera | @farkbarn <frankb2320@gmail.com>
2015-08-01 18:31:54 -0430

    correccion3

--- models/models.js
+++ models/models.js
@@ -3,7 +3,7 @@ var path = require('path');
 //SQLite: sqlite://:@:/
 
 //var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
-var url = process.env.DATABASE_URL.match((.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*));
+var url = process.env.DATABASE_URL.match((.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
 var DB_name = (url[6]||null);
 var user    = (url[2]||null);
 var pwd     = (url[3]||null);
